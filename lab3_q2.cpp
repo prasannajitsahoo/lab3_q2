f902700af8a8ef3999578f6d3c53d2b6f215e5b5
prasannajitsahoo <42238651+prasannajitsahoo@users.noreply.github.com>
2018-08-17 12:25:08 +0530

    version 1.4

---
+++ lab3_q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a=100;
+int b=10;
+int sum = a+b;
+int difference = a-b;
+int multiply = a*b;
+int divide =a/b;
+int c=11%5;
+cout<<"sum of a and b is "<< sum<<endl;
+cout<< "difference of a and b is "<< divide<<endl;
+cout<< "multiplication of a and b is "<<multiply <<endl;
+cout<< "division of a and b is "<<divide<<endl;
+cout<< "modulus of a and b is "<<c<<endl;
+return 1;
+}
+
